fe807c603a42233ecadbd54023acaae58a242a76
Filip Skibiński <filipwski@gmail.com>
2021-12-19 11:33:11 +0000

    move List component; integrate react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,8 @@
-import LoadingOverlay from 'react-loading-overlay-ts';
-import { BounceLoader } from 'react-spinners';
-import { useMembersQuery } from "./generated/graphql";
+import { ContributorsListView } from "./components/ContributorsListView/ContributorsListView";
+import { Routes, Route } from 'react-router-dom'; 
 
-export const App = () => {
-  const { error, data, fetchMore, loading } = useMembersQuery({ notifyOnNetworkStatusChange: true });
-
-  if (error) return <p>{error}</p>;
-
-  const members = data?.organization?.membersWithRole?.nodes;
-  const cursor = data?.organization?.membersWithRole?.pageInfo.endCursor;
-
-  return (
-    <LoadingOverlay
-      active={loading}
-      spinner={<BounceLoader />}
-    >
-      {members?.map(
-        (element) => <p key={element?.id} >{element?.name}</p>
-      )}
-      <button
-        onClick={() => {
-          fetchMore({
-            variables: { cursor },
-            updateQuery: (previousResults, { fetchMoreResult }) => {
-              const oldMemberValues = previousResults.organization?.membersWithRole.nodes;
-              const newMemberValues = fetchMoreResult?.organization?.membersWithRole.nodes;
-              if (!newMemberValues || !oldMemberValues) return previousResults;
-
-              return {
-                organization: {
-                  membersWithRole: {
-                    pageInfo: {
-                      endCursor: fetchMoreResult?.organization?.membersWithRole.pageInfo?.endCursor,
-                    },
-                    nodes: [...oldMemberValues, ...newMemberValues],
-                  }
-                }
-              };
-            },
-          })
-      }}
-      >
-        Fetch more
-      </button>
-    </LoadingOverlay>
-  );
-};
+export const App = () => (
+  <Routes>
+    <Route path="/" element={<ContributorsListView />} />
+  </Routes>
+);
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import { StrictMode } from 'react'
 import { render } from 'react-dom';
 import { App } from './App';
 import { client } from './apiClient/client';
+import { BrowserRouter } from "react-router-dom";
 
 render(
   <StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   </StrictMode>,
   document.getElementById('root')
 );
