be92dfa471cf96c22f287b97ca14d31ca961bd8a
Filip Skibiński <filipwski@gmail.com>
2021-12-23 01:22:46 +0000

    use general table width

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,6 +1,6 @@
 import './Table.styles.css';
 import { Cell, Column, useSortBy, useTable } from 'react-table';
-import { Dictionary } from 'lodash';
+import { Dictionary, isEmpty } from 'lodash';
 
 export type TableDictionary = Dictionary<string | number | null | TableDictionary>;
 
@@ -18,41 +18,46 @@ export const Table = <T extends TableDictionary>({ className, columns, data, ren
 
   /* eslint-disable react/jsx-key */
   return (
-    <table {...getTableProps({ className })}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render('Header')}
-                <span>
-                  {column.isSorted
-                    ? column.isSortedDesc
-                      ? ' ▼'
-                      : ' ▲'
-                    : ''}
-                </span>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => (
-                <td {...cell.getCellProps()}>
-                  {
-                    (renderCell && renderCell(cell)) ?? cell.render('Cell')
-                  }
-                </td>
+    <>
+      <table {...getTableProps({ className })}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? ' ▼'
+                        : ' ▲'
+                      : ''}
+                  </span>
+                </th>
               ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => (
+                  <td {...cell.getCellProps()}>
+                    {
+                      (renderCell && renderCell(cell)) ?? cell.render('Cell')
+                    }
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {isEmpty(data) && (
+        <p className="no-data-text">No data to display...</p>
+      )}
+    </>
   );
 };
